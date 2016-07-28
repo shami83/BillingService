34fa2b463e67dc83f6ca6935b38d920d2bc64050
shami83 <Shamik>
2016-07-28 19:26:14 +0530

    add Item in Catalog

--- src/main/java/com/microservice/billing/utility/PriceCatalog.java
+++ src/main/java/com/microservice/billing/utility/PriceCatalog.java
@@ -22,6 +22,7 @@ public class PriceCatalog {
 		priceMap.put("ac", "30000");
 		priceMap.put("smartphone", "14000");
 		priceMap.put("radio", "2000");
+		priceMap.put("musicbox", "20000");
 		
 		
 	}
